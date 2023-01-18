05a437414bc20ca837c157a7e710e615a6a979a6
chibuzodaniel <chibuzodaniel71@gmail.com>
2023-01-18 04:30:01 +0100

    initial commit

---
+++ centrixss.js
@@ -0,0 +1,8 @@
+let  Navbar = document.querySelector('.navbar');
+let Fabars = document.querySelector('.fa-bars');
+
+Fabars.onclick = () => {
+    Navbar.style.top = "7.5rem";
+    // Navbar.classList.toggle("active");
+}
+    
